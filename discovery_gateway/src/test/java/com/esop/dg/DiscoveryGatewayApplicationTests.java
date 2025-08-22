0eb31a413af6970af83b80bbb84edc9a38e45b16
Prakash Manna <prakashmanna33@gmail.com>
2025-08-22 16:48:24 +0530

    add discovery gateway project

---
+++ discovery_gateway/src/test/java/com/esop/dg/DiscoveryGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esop.dg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
